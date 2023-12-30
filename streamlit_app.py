dfe4bb25fb96512ee74229cccb26fe8d8e1b6654
DikshyantAcharya <dikshyant.acharya@trivelope.de>
2023-12-31 00:41:35 +0100

    first commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,3 +21,48 @@ import numpy as np
 tokenizer = Tokenizer.from_pretrained("bert-base-cased")
 
 DB_PATH = os.environ.get("DB_PATH", os.getcwd() + "/data-par_mul-6000-optimized/chromadb")
+COLLECTION_NAME = os.environ.get("COLLECTION_NAME", "Dikshyant-Acharya")
+inference_api_url = 'https://em-german-70b.llm.mylab.th-luebeck.dev'
+
+persistent_client = chromadb.PersistentClient(path=DB_PATH)
+collection = persistent_client.get_or_create_collection(COLLECTION_NAME)
+
+model = 'sentence-transformers/paraphrase-multilingual-mpnet-base-v2'
+class MyEmbeddingFunction(EmbeddingFunction):
+    def __init__(self):
+        # Initialize your sentence transformer model here
+        self.model = SentenceTransformer(model)
+
+    def embed_documents(self, texts):
+        # Generate embeddings using the sentence transformer model
+        embeddings = self.model.encode(texts, show_progress_bar= True)
+
+        # Convert numpy array to a list of lists
+        embeddings_list = embeddings.tolist()
+
+        return embeddings_list
+
+    def embed_query(self, query):
+        # Embed a single query string
+        embedding = self.model.encode([query], show_progress_bar=True)
+
+        # Flatten the embedding if it's not already a 1-dimensional list
+        if isinstance(embedding, list):
+            # If it's a list of lists, flatten it
+            if all(isinstance(elem, list) for elem in embedding):
+                embedding = [item for sublist in embedding for item in sublist]
+        elif isinstance(embedding, np.ndarray):
+            # If it's an ndarray, convert it to a flat list
+            embedding = embedding.flatten().tolist()
+
+        return embedding
+
+
+    def __call__(self, input: Documents) -> Embeddings:
+        # Convert input documents to a list of strings
+        document_texts = [doc.text for doc in input]
+
+        # Call embed_documents
+        return self.embed_documents(document_texts)
+
+
